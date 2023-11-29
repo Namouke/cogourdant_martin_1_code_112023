1bf9a5aa61c1ad60c4e3940ae736e371b10fc198
Namouke <79513377+Namouke@users.noreply.github.com>
2023-11-29 11:33:20 +0100

    mise à jour commentaire

--- controllers/User.js
+++ controllers/User.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-
+// Middleware de création de compte utilisateur
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
@@ -20,6 +20,7 @@ exports.signup = (req, res, next) => {
         });
 };
 
+// Middleware de connexion de compte d'un utilisateur
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
